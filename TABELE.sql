16d50326a50b3c6b4a5125053cad628b4b00f8f8
Wera <woziemniak@gmail.com>
2017-12-03 02:32:48 +0100

    Add all tables needed in project

---
+++ TABELE.sql
@@ -0,0 +1,181 @@
+-- DATABASE
+DROP DATABASE `BookstorCZ`;
+CREATE DATABASE IF NOT EXISTS `BookstorCZ`;
+USE `BookstorCZ`;
+
+-- KSIĄŻKI
+CREATE TABLE IF NOT EXISTS `Książki` (
+  `ISBN` VARCHAR(30),
+  `tytuł` VARCHAR(30),
+  `autor` VARCHAR(30),
+  `dział` VARCHAR(30),
+  `liczba` INT,
+  `wydawnictwo` VARCHAR(30),
+  `rokWydania` INT,
+  `cena` INT,
+  `opis` VARCHAR(30),
+  PRIMARY KEY (`ISBN`),
+  FOREIGN KEY (`autor`) REFERENCES `Autorzy` (`ID`)
+);
+
+-- AUTORZY
+CREATE TABLE IF NOT EXISTS `Autorzy` (
+  `ID` INT,
+  `imię` VARCHAR(30),
+  `nazwisko` VARCHAR(30),
+  PRIMARY KEY (`ID`)
+);
+
+-- DZIAŁYPOM
+CREATE TABLE IF NOT EXISTS `DziałyPom` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `ISBN` VARCHAR(30),
+  `IDdział` INT,
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`ISBN`) REFERENCES `Książki` (`ISBN`),
+  FOREIGN KEY (`IDdział`) REFERENCES `Działy` (`ID`)
+);
+
+-- DZIAŁY
+CREATE TABLE IF NOT EXISTS `Działy` (
+  `ID` INT,
+  `nazwa` VARCHAR(30),
+  PRIMARY KEY (`ID`)
+);
+
+-- ZAMÓWIONE KSIĄŻKI
+CREATE TABLE IF NOT EXISTS `ZamówioneKsiążki`(
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `ISBN` VARCHAR(30),
+  `IDzamówienia` INT,
+  `liczba` INT,
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`IDzamówienia`) REFERENCES `Zamówienia` (`ID`),
+  FOREIGN KEY (`ISBN`) REFERENCES `Książki` (`ISBN`)
+);
+
+-- ZAMÓWIENIA
+CREATE TABLE IF NOT EXISTS `Zamówienia` (
+  `ID` INT,
+  `IDzamawiającego` INT, 
+  `dataZamówienia` DATE,
+  `status` ENUM('Złożone', 'Wysłane', 'Dostarczone'),
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`IDzamawiającego`) REFERENCES `Klienci` (`ID`)
+);
+
+-- KLIENCI
+CREATE TABLE IF NOT EXISTS `Klienci` (
+  `ID` INT,
+  `imię` VARCHAR(30),
+  `nazwisko` VARCHAR(30), 
+  `adres` INT,
+  `telefon` VARCHAR(30),
+  `mail` VARCHAR(30),
+  `login` VARCHAR(30),
+  `hasło` VARCHAR(30),
+  PRIMARY KEY (`ID`), 
+  FOREIGN KEY (`adres`) REFERENCES `AdresyKlienci` (`ID`)
+);
+
+-- ADRESY KLIENCI
+CREATE TABLE IF NOT EXISTS `AdresyKlienci` (
+  `ID` INT,
+  `ulica` VARCHAR(30),
+  `numer lokalu` VARCHAR(30),
+  `kodPocztowy` VARCHAR(30),
+  `miejscowość` VARCHAR(30),
+  `województwo` VARCHAR(30),
+  `kraj` VARCHAR(30),
+  PRIMARY KEY (`ID`)
+);
+
+-- PRACOWNICY
+CREATE TABLE IF NOT EXISTS `Pracownicy` (
+  `ID` INT,
+  `imię` VARCHAR(30),
+  `nazwisko` VARCHAR(30), 
+  `adres` INT,
+  `telefon` VARCHAR(30),
+  `mail` VARCHAR(30),
+  `login` VARCHAR(30),
+  `hasło` VARCHAR(30),
+  PRIMARY KEY (`ID`), 
+  FOREIGN KEY (`adres`) REFERENCES `AdresyPracownicy` (`ID`)
+);
+
+-- ADRESY PRACOWNICY
+CREATE TABLE IF NOT EXISTS `AdresyPracownicy` (
+  `ID` INT,
+  `ulica` VARCHAR(30),
+  `numer lokalu` VARCHAR(30),
+  `kodPocztowy` VARCHAR(30),
+  `miejscowość` VARCHAR(30),
+  `województwo` VARCHAR(30),
+  `kraj` VARCHAR(30),
+  PRIMARY KEY (`ID`)
+);
+
+-- DOSTAWCY
+CREATE TABLE IF NOT EXISTS `Dostawcy` (
+  `NIP` VARCHAR(30),
+  `nazwaFirmy` VARCHAR(30),
+  `imięWłaściciela` VARCHAR(30),
+  `nazwiskoWłaściciela` VARCHAR(30), 
+  `adres` INT,
+  `telefon` VARCHAR(30),
+  `mail` VARCHAR(30),
+  `nrKonta` VARCHAR(30),
+  PRIMARY KEY (`NIP`), 
+  FOREIGN KEY (`adres`) REFERENCES `AdresyDostawcy` (`ID`)
+);
+
+-- DOSTAWCY ADRESY
+CREATE TABLE IF NOT EXISTS `AdresyKlienci` (
+  `ID` INT,
+  `ulica` VARCHAR(30),
+  `numer lokalu` VARCHAR(30),
+  `kodPocztowy` VARCHAR(30),
+  `miejscowość` VARCHAR(30),
+  `województwo` VARCHAR(30),
+  `kraj` VARCHAR(30),
+  PRIMARY KEY (`ID`)
+);
+
+-- DOSTAWY
+CREATE TABLE IF NOT EXISTS `Dostawy` (
+  `ID` INT,
+  `IDdostawcy` INT,
+  `NIP` VARCHAR(30),
+  `nrFaktury` VARCHAR(30),
+  `dataDostawy` DATE,
+  `status` ENUM('Złożono', 'Wysłano', 'Dostarczono'),
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`IDdostawcy`) REFERENCES `Dostawcy` (`NIP`)
+);
+
+-- DOTOWAROWANIE
+CREATE TABLE IF NOT EXISTS `Dotowarowanie` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `ISBN` VARCHAR(30),
+  `IDdostawy` INT,
+  `liczba` INT,
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY (`ISBN`) REFERENCES `Książki` (`ISBN`),
+  FOREIGN KEY (`IDdostawy`) REFERENCES `Dostawy` (`ID`)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
