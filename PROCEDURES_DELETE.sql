d3d92ed6fa8fabc8b86646c61299e8aa5ab60a31
Wera <woziemniak@gmail.com>
2017-12-09 01:19:56 +0100

    Add PROCEDURES_DELETE - procedures responsible for delete a single row

---
+++ PROCEDURES_DELETE.sql
@@ -0,0 +1,22 @@
+USE `BookstorCZ`;
+
+-- USUŃ WIERSZ O PODANYM ID
+DROP PROCEDURE IF EXISTS usunWiersz;
+DELIMITER //
+CREATE PROCEDURE usunWiersz(IDUsun INT, tabela CHAR(20))
+BEGIN
+  IF EXISTS(SELECT * 
+			FROM information_schema.COLUMNS 
+			WHERE TABLE_SCHEMA = 'BookstoreCZ' 
+					AND TABLE_NAME = 'BookstoreCZ') THEN
+                     
+						SET @str = CONCAT('DELETE FROM ',tabela,' WHERE ',tabela,'.ID = IDUsun;');
+						PREPARE stmt FROM @str;
+						EXECUTE stmt;
+						DEALLOCATE PREPARE stmt;
+  ELSE
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Niepoprawna kolumna';
+  END IF;
+END //
+DELIMITER ;
+
