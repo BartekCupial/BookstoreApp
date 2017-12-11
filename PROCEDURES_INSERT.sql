98dfeece7be941c881af2db139bb3b591c31063e
bartek <BartekCupial>
2017-12-12 00:50:48 +0100

    srednio dzialajace procedury do wypelniania zamowionych ksiazek

--- PROCEDURES_INSERT.sql
+++ PROCEDURES_INSERT.sql
@@ -97,3 +97,24 @@ DECLARE CONTINUE HANDLER FOR NOT FOUND SET EOS = TRUE;
 END$$
 DELIMITER ;
 
+
+
+drop procedure wypelnijDotowarowanie;
+DELIMITER $$
+CREATE PROCEDURE wypelnijDotowarowanie(in ISBN varchar(30), in liczba int, in IDdostawy int)
+BEGIN
+DECLARE EOS BOOLEAN DEFAULT FALSE;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET EOS = TRUE;
+
+ -- tu on się wykona tylko raz ale zrobilem to tak, zeby cos bylo, potem wymyslimy jak to zrobic
+ -- na razie jest jakis szablon
+			cycle: LOOP
+                IF EOS
+				THEN LEAVE cycle;
+				END IF;
+                INSERT INTO `ZamówioneKsiążki` VALUES (0,ISBN, IDdostawy, liczba);
+			END LOOP cycle;
+	   COMMIT;    
+END$$
+DELIMITER ;
+
